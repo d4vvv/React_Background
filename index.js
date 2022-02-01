819f743722e0225168b7c33c6d6f4348b3399ecb
d4vvv <kacper.godula99@gmail.com>
2022-02-01 13:38:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.put('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id)
-    const note = notes.filter(n => n.id === id)
+    const note = notes.filter(n => n.id === id)[0]
     updatedNote = {...note, important: !note.important}
     notes = notes.map(n => n.id !== id ? n : updatedNote)
     
