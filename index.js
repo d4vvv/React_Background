299647f179cc5ebc965208996189d26272e5950a
d4vvv <kacper.godula99@gmail.com>
2022-02-04 22:21:26 +0100

    3.3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const { request, response } = require('express')
+const Note = require('./models/note')
 const app = express()
 
 app.use(cors())
@@ -9,7 +10,19 @@ app.use(express.static('build'))
 
 app.use(express.json())
 
-let notes = [  
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+
+app.use(errorHandler)
+
+/*let notes = [  
     {
             id: 1,
             content: "HTML is easy",    
@@ -26,7 +39,7 @@ let notes = [
             date: "2019-05-30T19:20:14.298Z",    
             important: true  
         }
-    ]
+    ]*/
     
 const generateId = () => {
     const maxId = notes.length > 0
@@ -41,57 +54,64 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {    
-        response.json(note)  
-    } else {    
-        response.status(404).end()  
-    }
+    Note.findById(request.params.id).then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
     
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = {
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing' })
+      }
+    
+      const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId()
-    }
-
-    notes = notes.concat(note)
-    response.json(note)
+      })
+    
+      note.save().then(savedNote => {
+        response.json(savedNote)
+      })
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+    Note.findByIdAndRemove(require.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
   })
 
 app.put('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.filter(n => n.id === id)[0]
-    const updatedNote = {...note, important: !note.important}
-    notes = notes.map(n => n.id !== id ? n : updatedNote)
-    
-    response.json(updatedNote)
+    const body = request.body
 
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+    
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
     
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
